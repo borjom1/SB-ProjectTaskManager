0a12ea145c043ecfc004b0cdcd14e084314be95c
Igor <ig.zuban1ch@gmail.com>
2023-06-17 23:48:07 +0300

    feat(ProjectTaskManagerApplication): add model mapper bean

--- src/main/java/com/example/projecttaskmanager/ProjectTaskManagerApplication.java
+++ src/main/java/com/example/projecttaskmanager/ProjectTaskManagerApplication.java
@@ -1,13 +1,20 @@
 package com.example.projecttaskmanager;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectTaskManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectTaskManagerApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
