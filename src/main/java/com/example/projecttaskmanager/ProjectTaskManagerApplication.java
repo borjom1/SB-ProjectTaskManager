e6370600051d957930a9f2cc8256ad2edce341da
Igor <ig.zuban1ch@gmail.com>
2023-06-17 12:57:39 +0300

    initial commit

---
+++ src/main/java/com/example/projecttaskmanager/ProjectTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecttaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projecttaskmanager/ProjectTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecttaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
