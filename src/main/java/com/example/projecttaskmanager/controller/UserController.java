96dc6de9b6049b23296fefd13932c7364a114fd0
Igor <ig.zuban1ch@gmail.com>
2023-06-20 21:37:09 +0300

    feat(UserController): add end-point for retrieving user data

--- src/main/java/com/example/projecttaskmanager/controller/UserController.java
+++ src/main/java/com/example/projecttaskmanager/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.example.projecttaskmanager.controller;
 
+import com.example.projecttaskmanager.dto.UserInfoDto;
 import com.example.projecttaskmanager.exception.UserNotFoundException;
 import com.example.projecttaskmanager.security.UserDetailsImpl;
 import com.example.projecttaskmanager.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/user")
@@ -22,10 +21,18 @@ public class UserController {
     @PatchMapping("/logout")
     @ResponseStatus(NO_CONTENT)
     public void logout() throws UserNotFoundException {
-        var principal = (UserDetailsImpl) SecurityContextHolder.getContext()
+        userService.logout(getPrincipal().getId());
+    }
+
+    @GetMapping
+    public UserInfoDto getUserInfo() throws UserNotFoundException {
+        return userService.getUserInfo(getPrincipal().getId());
+    }
+
+    private UserDetailsImpl getPrincipal() {
+        return (UserDetailsImpl) SecurityContextHolder.getContext()
                 .getAuthentication()
                 .getPrincipal();
-        userService.logout(principal.getId());
     }
 
 }
