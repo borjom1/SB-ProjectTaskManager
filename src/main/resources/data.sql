305c6583255aae704caf3e34f040fa0774d58d4e
Igor <ig.zuban1ch@gmail.com>
2023-06-23 01:47:05 +0300

    feat: add project entity to sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,9 +6,19 @@ values ('ROLE_USER'),
        ('ROLE_ADMIN');
 
 insert into users(first_name, last_name, login, password, avatar, position)
-values ('Mark', 'Zuckerberg', 'mark31', '$2a$12$6kHi8D0MDtARuXCRxZeRAuK5X5jojeahinZvBiHPGpkmuaK6F.rxG', '', '');
+values ('Mark', 'Zuckerberg', 'mark31', '$2a$12$6kHi8D0MDtARuXCRxZeRAuK5X5jojeahinZvBiHPGpkmuaK6F.rxG', DEFAULT, 'Java Software Engineer');
 
 insert into user_roles(user_id, role_id)
 values (1, 1),
        (1, 2),
        (1, 3);
+
+insert into projects(name, description, tasks, tasks_done)
+values (
+        'Mobile Banking App',
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        12,
+        7
+);
+
+insert into members(user_id, project_id) values (1, 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,5 @@
+drop table if exists members;
+drop table if exists projects;
 drop table if exists user_roles;
 drop table if exists roles;
 drop table if exists users;
@@ -10,7 +12,7 @@ create table users
     login         varchar(64) unique not null,
     password      text               not null,
     created_at    timestamptz        not null default now(),
-    avatar        text               not null,
+    avatar        text               not null default 'C:\Users\Igor\Desktop\project-task-manager\target\classes\avatars\default',
     position      varchar(128)       not null,
     projects      bigint             not null default 0,
     refresh_token text
@@ -27,4 +29,20 @@ create table user_roles
     user_id bigint references users (id),
     role_id bigint references roles (id),
     primary key (user_id, role_id)
+);
+
+create table projects
+(
+    id          bigserial primary key,
+    name        varchar(128) not null,
+    description text         not null,
+    tasks       bigint       not null default 0,
+    tasks_done  bigint       not null default 0
+);
+
+create table members
+(
+    user_id    bigint,
+    project_id bigint,
+    primary key (user_id, project_id)
 );
