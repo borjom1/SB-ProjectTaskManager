669c90bde595bf570a2e23c7bef219127b0f050b
Igor <ig.zuban1ch@gmail.com>
2023-06-24 23:16:16 +0300

    feat(ProjectController): add end-points for tasks and stories

--- src/main/java/com/example/projecttaskmanager/controller/ProjectController.java
+++ src/main/java/com/example/projecttaskmanager/controller/ProjectController.java
@@ -2,6 +2,10 @@ package com.example.projecttaskmanager.controller;
 
 import com.example.projecttaskmanager.dto.NewProjectDto;
 import com.example.projecttaskmanager.dto.ProjectDto;
+import com.example.projecttaskmanager.dto.StoryDto;
+import com.example.projecttaskmanager.dto.TaskDto;
+import com.example.projecttaskmanager.exception.FakeMemberException;
+import com.example.projecttaskmanager.exception.StoryNotFoundException;
 import com.example.projecttaskmanager.exception.UserNotFoundException;
 import com.example.projecttaskmanager.security.UserDetailsImpl;
 import com.example.projecttaskmanager.service.ProjectService;
@@ -20,6 +24,11 @@ public class ProjectController {
 
     private final ProjectService projectService;
 
+    @GetMapping("/{id}")
+    public ProjectDto getProject(@PathVariable Long id) throws UserNotFoundException, FakeMemberException {
+        return projectService.getProject(id, getPrincipal().getId());
+    }
+
     @GetMapping
     public List<ProjectDto> getProjects() throws UserNotFoundException {
         return projectService.getProjects(getPrincipal().getId());
@@ -29,6 +38,22 @@ public class ProjectController {
     @RolesAllowed({"MANAGER", "ADMIN"})
     public ProjectDto createProject(@Valid @RequestBody NewProjectDto dto) throws UserNotFoundException {
         return projectService.createProject(dto, getPrincipal().getId());
+    }
+
+    @GetMapping("/tasks")
+    public List<TaskDto> getTasksWith(@RequestParam Long projectId, @RequestParam Long storyId)
+            throws UserNotFoundException, FakeMemberException, StoryNotFoundException {
+        return projectService.getTasks(projectId, storyId, getPrincipal().getId());
+    }
+
+    @GetMapping("/{id}/story")
+    public StoryDto getFirstStory(@PathVariable Long id) throws UserNotFoundException, FakeMemberException {
+        return projectService.getFirstStory(id, getPrincipal().getId());
+    }
+
+    @GetMapping("/{id}/stories")
+    public List<StoryDto> getStories(@PathVariable Long id) throws UserNotFoundException, FakeMemberException {
+        return projectService.getStories(id, getPrincipal().getId());
     }
 
     private UserDetailsImpl getPrincipal() {
