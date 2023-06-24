21287fa36b8df76666793724636ee90a13f12b42
Igor <ig.zuban1ch@gmail.com>
2023-06-24 23:13:38 +0300

    feat(JwtFilter): add request uri to logging

--- src/main/java/com/example/projecttaskmanager/security/jwt/JwtFilter.java
+++ src/main/java/com/example/projecttaskmanager/security/jwt/JwtFilter.java
@@ -30,7 +30,7 @@ public class JwtFilter extends OncePerRequestFilter {
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        log.info("-> doFilterInternal()");
+        log.info("-> doFilterInternal(): path={}", request.getRequestURI());
 
         String authHeader = request.getHeader("Authorization");
         String token = jwtProvider.getToken(authHeader);
