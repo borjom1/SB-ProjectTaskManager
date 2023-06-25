b300da0f536e1f1264265dbde726a14bbebab77f
Igor <ig.zuban1ch@gmail.com>
2023-06-26 00:40:21 +0300

    feat: add exception

---
+++ src/main/java/com/example/projecttaskmanager/exception/TaskNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.projecttaskmanager.exception;
+
+public class TaskNotFoundException extends Exception {
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+}
