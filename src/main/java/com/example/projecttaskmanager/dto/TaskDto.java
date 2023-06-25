81df9ca050b240d8313d905666b0e60968906332
Igor <ig.zuban1ch@gmail.com>
2023-06-25 15:27:52 +0300

    refactor(TaskDto): add setters

--- src/main/java/com/example/projecttaskmanager/dto/TaskDto.java
+++ src/main/java/com/example/projecttaskmanager/dto/TaskDto.java
@@ -4,12 +4,14 @@ import com.example.projecttaskmanager.entity.TaskStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class TaskDto {
     private Long id;
     private String title;
